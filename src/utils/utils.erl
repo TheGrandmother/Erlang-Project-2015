fc5b6cee0174d5bd8d8f7082c9899b8b1e71483b
Oscar Wallster <oscarw_91@hotmail.com>
2015-05-05 15:29:10 +0200

    get directions from nighbourhood added

--- src/utils/utils.erl
+++ src/utils/utils.erl
@@ -1,32 +1,70 @@
 -module(utils).
--import(test,[cellstarter/0]).
--import(silly,[testShit/0,get2D/2,coolPrint/2]).
--import_type(types,[cell/0]). 
--export([getHood/1,setHood/2,getPos/1,setPos/2, getPid/1,getNext/1,getAttributes/1,setAttributes/2,getMetadata/1,getProperty/2]).
+-export([getCell_Hood/1,setCell_Hood/2,getCell_Pos/1,setCell_Pos/2, getCell_Pid/1,setCell_Pid/2,getCell_Next/1,setCell_Next/2,getCell_Attributes/1,setCell_Attributes/2,getCell_Metadata/1,setCell_Metadata/2,getProperty/2,initCell/1,getOneDirection/2]).
 
+%%initCell -> Cell
+initCell(Position = {_X,_Y}) ->
+    {self(), Position,none,none,none,none}.
+
+%%Get and setters 0f nichghbourhood
+getOneDirection(Cell,northwest) ->
+    {NW,_,_,_,_,_,_,_,_} = getCell_Hood(Cell),
+    NW;
+getOneDirection(Cell,north) ->
+    {_,N,_,_,_,_,_,_,_} = getCell_Hood(Cell),
+    N;
+getOneDirection(Cell,northeast) ->
+    {_,_,NE,_,_,_,_,_,_} = getCell_Hood(Cell),
+    NE;
+getOneDirection(Cell,west) ->
+    {_,_,_,W,_,_,_,_,_} = getCell_Hood(Cell),
+    W;
+getOneDirection(Cell,center) ->
+    {_,_,_,_,C,_,_,_,_} = getCell_Hood(Cell),
+    C;
+getOneDirection(Cell, east) ->
+    {_,_,_,_,_,E,_,_,_} = getCell_Hood(Cell),
+    E;
+getOneDirection(Cell, southwest) ->
+    {_,_,_,_,_,_,SW,_,_} = getCell_Hood(Cell),
+    SW;
+getOneDirection(Cell, south) ->
+    {_,_,_,_,_,_,_,S,_} = getCell_Hood(Cell),
+    S;
+getOneDirection(Cell, southeast) ->
+    {_,_,_,_,_,_,_,_,SE} = getCell_Hood(Cell),
+    SE;
+getOneDirection(_Cell, _) ->
+    none.
 
 %%Get and setters of Cell type
-getHood(Cell = {_,_,Hood,_,_,_}) ->
+getCell_Hood(_Cell = {_,_,Hood,_,_,_}) ->
     Hood.
-setHood(Cell = {Pid, Position,_,Next_Cell,Attributes,Metadata}, New_Hood) ->
+setCell_Hood(_Cell = {Pid, Position,_,Next_Cell,Attributes,Metadata}, New_Hood) ->
     {Pid,Position,New_Hood, Next_Cell,Attributes,Metadata}.
-getPos(Cell = {_,Pos = {X,Y},_,_,_,_}) ->
+getCell_Pos(_Cell = {_,Pos = {_X,_Y},_,_,_,_}) ->
     Pos.
-setPos(Cell = {Pid,_,Hood, Next_Cell, Attributes, Metadata},New_Pos = {X,Y}) ->   
+setCell_Pos(_Cell = {Pid,_,Hood, Next_Cell, Attributes, Metadata},New_Pos = {_X,_Y}) ->   
     {Pid, New_Pos, Hood, Next_Cell, Attributes, Metadata}.
-getPid(Cell = {Pid,_,_,_,_,_}) ->
+getCell_Pid(_Cell = {Pid,_,_,_,_,_}) ->
     Pid.
-getNext(Cell = {_,_,_,Next_Cell,_,_}) ->
+setCell_Pid(_Cell = {_,Position, Hood,Next_Cell, Attributes, Metadata},New_Pid) ->
+    {New_Pid,Position,Hood,Next_Cell,Attributes, Metadata}.
+getCell_Next(_Cell = {_,_,_,Next_Cell,_,_}) ->
     Next_Cell.
-getAttributes(Cell = {_,_,_,_,Attributes,_}) ->
+setCell_Next(_Cell = {Pid,Position,Hood, _, Attributes, Metadata},New_Nextcell) ->
+    {Pid,Position,Hood, New_Nextcell, Attributes, Metadata}.
+getCell_Attributes(_Cell = {_,_,_,_,Attributes,_}) ->
     Attributes.
-setAttributes(Cell = {Pid,Position,Hood, Next_Cell, _, Metadata}, New_Attr) ->
+setCell_Attributes(_Cell = {Pid,Position,Hood, Next_Cell, _, Metadata}, New_Attr) ->
     {Pid,Position,Hood, Next_Cell,New_Attr,Metadata}.
-getMetadata(Cell = {_,_,_,_,_,Metadata}) ->
+getCell_Metadata(_Cell = {_,_,_,_,_,Metadata}) ->
     Metadata.
+setCell_Metadata(_Cell = {Pid,Position,Hood, Next_Cell, Attributes, _}, New_metadata) ->
+    {Pid,Position,Hood,Next_Cell,Attributes,New_metadata}.
+
 %%getProperty/2
 % Returns the Tuple in list that matches property.
-getProperty([],Property) ->
+getProperty([],_Property) ->
     none;
 getProperty([(L = {Type, _Val})],Property) ->
     if
