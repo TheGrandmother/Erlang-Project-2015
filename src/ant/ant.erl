5467984c567b3f62a1e47d8022ff4219f8d079f8
Aleksander Lundqvist <kafasius@gmail.com>
2015-05-11 22:42:38 +0000

    Added logging to ant, changed implementation test

--- src/ant/ant.erl
+++ src/ant/ant.erl
@@ -16,14 +16,17 @@ spawn_Ant(Cell_Pid, Attributes) ->
 ant_Init(Cell_Pid, Attributes) ->
 	Ant_ID = self(),
 	State = searching_for_food,
+	Log = logger:makeLog(logger:makeCoolString("Ant ~p",[Ant_ID]),self()),
 	Buffer = {0, []},  
 	Ref = make_ref(),
-	Cell_Pid ! {Ant_ID, Ref, {place_ant, Ant_ID}},
+	Msg = Cell_Pid ! {Ant_ID, Ref, {place_ant, Ant_ID}},
 	{{Sender, _, _, Response}, New_Buffer} = message_buffer:receiver(Ref, Buffer),
-	Ant = {self(), Cell_Pid, State, Attributes, New_Buffer},
+	Ant = {self(), Cell_Pid, State, Attributes, New_Buffer, Log},
+	logger:logEvent(utils:getAntLog(Ant),"Ant spawned."),
+	logger:logMessageSent(utils:getAntLog(Ant),Msg,Cell_Pid),
 	case Response of
 		{place_ant_reply, fail} ->
-			io:format("Ant placement failed~n"),
+			logger:logEvent(utils:getAntLog(Ant),"Ant failed to get placed and is killed."),
 			exit(fail);
 		{place_ant_reply, sucsess} ->
 			if 
@@ -31,10 +34,12 @@ ant_Init(Cell_Pid, Attributes) ->
 				   io:format("Responder is not given cell ~n"),
 				   exit(fail);
 				true ->
+					logger:logEvent(utils:getAntLog(Ant),"Ant successfully placed in "),
+					logger:logEvent(utils:getAntLog(Ant),Cell_Pid),
 					ant_Main(Ant)
 			end;
 		_ ->
-			io:format("Unexpected response during ant placement~n"),
+			logger:logEvent(utils:getAntLog(Ant),"Ant fucked up."),
 			exit(fail)
 	end.
 
@@ -46,25 +51,31 @@ ant_Main(Ant) ->
 	State = utils:getAntState(Ant),
 	Cell = utils:getAntCell(Ant),
 	%% Tons of printout 
-	io:format("Ant in cell ~p ~n",[Cell]),
+	logger:logEvent(utils:getAntLog(Ant),(Cell)),
 	Buffer = utils:getAntMetadata(Ant),
 	case State of
 		searching_for_food ->
+			logger:logEvent(utils:getAntLog(Ant),"Ant is searching for food"),
 			Directions = [northwest, north, northeast, west, east, southwest, south, southeast],
 			Magic = random:uniform(8),
 			Direction = lists:nth(Magic, Directions),
+			logger:logEvent(utils:getAntLog(Ant),"Ant trying to move"),
+			logger:logEvent(utils:getAntLog(Ant),Direction),
 			Ref = make_ref(), 
-			Cell ! {self(), Ref, {move_ant, Direction}},
+			Msg = Cell ! {self(), Ref, {move_ant, Direction}},
+			logger:logMessageSent(utils:getAntLog(Ant),Msg,Cell),
 			{Message, New_Buffer} = message_buffer:receiver(Ref,Buffer),
 			New_Ant = utils:setAntMetadata(Ant, New_Buffer),
 			case Message of
 				{_, _, _,{move_ant_reply,{sucsess, Dest}}} ->
 					Newer_Ant = utils:setAntCell(New_Ant, Dest),
+					logger:logEvent(utils:getAntLog(Ant),"Ant is moving"),
 					ant_Main(Newer_Ant);
 				{_, _, _,{move_ant_reply,{fail, _}}} ->
+					logger:logEvent(utils:getAntLog(Ant),"Ant failed to move~n"),
 					ant_Main(New_Ant);
 				_ ->
-					io:format("Unexpected response during ant movement ~n"),
+					logger:logEvent(utils:getAntLog(Ant),"Ant got fucked up~n"),
 					ant_Main(New_Ant)
 			end;
 		_ ->
@@ -72,12 +83,12 @@ ant_Main(Ant) ->
 	end.
 
 
-
-
-%% Spawns a 1 by 2 grid and places an ant
+%% Spawns a 1 by 2 grid and places an ant that runs around for 5 seconds
 spawn_test() ->
 	Grid = grid_init:initGrid({2,1}),
 	Cell_ID = grid_init:getGridElement({0,0},{2,1},Grid),
-	spawn_Ant(Cell_ID, []).
+	Ant = spawn_Ant(Cell_ID, []),
+	timer:sleep(5000),
+	exit(Ant, kill).
 
 
