63ef0e6efd9e6c90ddbcc8dbbc6963fb9b0e81ba
ludstr <ludvig.stromberg@gmail.com>
2015-05-20 15:36:24 +0200

    Updated!

--- src/GUI/pyGUI.py
+++ src/GUI/pyGUI.py
@@ -1,9 +1,15 @@
-import pygame
+import pygame, sys, random
+from pygame.locals import *
+from erlport.erlterms import Atom,List
+from erlport.erlang import set_message_handler, cast
  
 ANT = (0, 0, 0) #1
 PLAIN = (50, 100, 0) #2 gräääs
+PLAIN1 = pygame.image.load('grass.jpg')
 FOOD = (100, 50, 0) #3 Maaat
 FOODANT = (0, 0, 255) #hungrigMYRA
+
+
 
 NEST = (0, 255, 0) 
 FEROMONE = (255, 0, 0)
@@ -15,8 +21,29 @@ MARGIN = 0
 
 gridArray = []
 
-testList = ["food", "ant", "foodant", "plain", "foodant", "food", "plain", "plain", "plain", "foodant", "foodant", "ant", "plain", "plain", "plain", "food", "ant", "plain", "plain", "foodant", "food", "ant", "ant", "plain", "food"]
+testList = ["plain", "ant", "plain", "foodant", "foodant", "foodant", "plain", "plain", "foodant", "foodant", "foodant", "ant", "foodant", "plain", "plain", "plain", "ant", "plain", "plain", "foodant", "food", "plain", "foodant", "plain", "food", "plain", "ant", "plain", "plain", "foodant", "plain", "plain", "plain", "foodant", "foodant", "foodant", "ant", "plain", "foodant", "plain", "plain", "ant", "plain", "plain", "plain", "food", "plain", "plain", "plain", "food", "foodant", "ant", "plain", "foodant", "plain", "plain", "plain", "plain", "plain", "foodant", "foodant", "ant", "plain", "plain", "foodant", "plain", "ant", "foodant", "plain", "foodant", "food", "plain", "foodant", "plain", "food", "foodant", "ant", "foodant", "plain", "foodant", "foodant", "foodant", "plain", "foodant", "foodant", "foodant", "ant", "plain", "plain", "foodant", "foodant", "ant", "foodant", "plain", "foodant", "food", "plain", "foodant", "plain", "food"]
 
+
+def randomList(amount):
+    
+    variables = ['food', 'plain', 'foodant', 'plain', 'plain', 'plain', 'plain', 'plain']
+    #global randList
+    randList = []
+
+    for x in range (amount):
+        randList.append([])
+
+    y = len(randList)
+    print(y)
+
+    for y in range (amount):
+        randList[y] = random.choice(variables)
+
+   
+    return randList
+
+#xSize = 99
+#ySize = 99
 
 def createGrid(x , y): 
  
@@ -24,7 +51,11 @@ def createGrid(x , y):
    global ySize
    xSize = x
    ySize = y
-   
+ 
+   print("created grid")
+   print(xSize)
+   print(ySize)
+  
    for row in range(xSize):
        
         gridArray.append([])
@@ -33,11 +64,11 @@ def createGrid(x , y):
 
 def updateGrid(Grid,List):
 
-    for row in range (xSize):
+   for row in range (xSize):
 
-        for column in range (ySize):
+      for column in range (ySize):
             
-            gridArray[row][column] = updateGridAux(List[column + row * ySize])
+         gridArray[row][column] = updateGridAux(List[column + row * ySize])
 
 
 def updateGridAux(input):
@@ -55,32 +86,9 @@ def updateGridAux(input):
         return 4
 
 
-
-def drawGrid(list):
-
-    pygame.init()
-
-    windowSize = [1000, 1000]
-    display = pygame.display.set_mode(windowSize)
-
-    pygame.display.set_caption("ANTS ARE LIFE")
-
-    done = False
-
-    clock = pygame.time.Clock()
-
-    updateGrid(gridArray, list)
+def drawGridAux():
     
-    
-
-    while not done:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-
-        display.fill(PLAIN)
-                
+                    
         for row in range(xSize):
             for column in range(ySize):
                 cellType = PLAIN
@@ -95,12 +103,15 @@ def drawGrid(list):
 
                 elif gridArray[row][column] == 2:
                     cellType = PLAIN
-                    pygame.draw.rect(display,
-                             cellType,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
+                    display.blit(PLAIN1, (column * WIDTH, row * HEIGHT))
+                    
+                    
+#                    pygame.draw.rect(display,
+ #                            cellType,
+  #                           [(MARGIN + WIDTH) * column + MARGIN,
+   #                           (MARGIN + HEIGHT) * row + MARGIN,
+    #                          WIDTH,
+     #                         HEIGHT])
                 
                 elif gridArray[row][column] == 3:
                     cellType = FOOD
@@ -119,20 +130,37 @@ def drawGrid(list):
                               WIDTH,
                               HEIGHT])
                 
-                                
-                
 
     
-        clock.tick(60)
 
-        pygame.display.flip()
+def register_handler(dest):
 
-createGrid(5, 5)
-drawGrid(testList)
+    pygame.init()
 
+    windowSize = [1000, 1000]
+    global display
+    display = pygame.display.set_mode(windowSize)
 
-pygame.quit()
+    pygame.display.set_caption("ANTS ARE LIFE")
+    display.fill(PLAIN)
 
+    clock = pygame.time.Clock()
+    clock.tick(60)
+    set_message_handler(handler)
+    print("WE ARE HERE NOW :(")
+    
+
+def handler(message):
+    
+    print(List.to_string(list(message)[0]))
+    Lol = list(map(lambda x : List.to_string(x),list(message)))
+    print(Lol)
+    updateGrid(gridArray, Lol) 
+
+   
+    drawGridAux()
+    pygame.display.flip()
+            
 
 
 
