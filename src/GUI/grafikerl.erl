18996d064821361c2b7ab2e833f2190375b58ba7
ludstr <ludvig.stromberg@gmail.com>
2015-05-20 15:37:56 +0200

    Updated!

--- src/GUI/grafikerl.erl
+++ src/GUI/grafikerl.erl
@@ -1,6 +1,13 @@
--module(grafikerl). 
--export([runPythonInstance/0]). 
-
-runPythonInstance() -> 
-
-    {ok, P} = python:start([{python, "python3"}]), python:call(P, grafikpy, drawAnt, []).
+-Module(grafikerl).
+-export([runPythonInstance/0]).
+ 
+-import(gui, [test/1]).
+ 
+runPythonInstance() ->
+ 
+    {ok, P} = python:start([{python, "python3"}]),
+    python:call(P, nr1, createGrid, [5, 5]),
+    python:call(P, nr1, register_handler, [self()]),
+    python:cast(P, test({5,5})),
+    timer:sleep(5000),
+    python:cast(P, ["food", "foodant", "foodant", "ant", "foodant", "food", "plain", "plain", "plain", "ant", "foodant", "ant", "foodant", "ant", "plain", "food", "ant", "plain", "plain", "foodant", "food", "foodant", "foodant", "ant", "food"]).
